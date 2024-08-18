127d336f1f0508cc17fffe9ddf24245d2514eb95
juwens <>
2024-08-18 14:21:42 +0200

    iterate mobileprovision files in Library/MobileDevice/Provisioning Profiles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let profiles_dir = dirs::home_dir().unwrap().join("Library/MobileDevice/Provisioning Profiles");
+
+    let paths = fs::read_dir(profiles_dir).unwrap()
+        .map(|dir_entry| dir_entry.unwrap().path())
+        .filter_map(|path| {
+            if path.extension().map_or(false, |ext| ext == "mobileprovision") {
+                Some(path)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+
+    for path in paths {
+        println!("Name: {}", path.display())
+    }
 }
