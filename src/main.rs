a8c97f66ffe21de8e0205f082138f43ae904b84f
juwens <>
2024-08-25 18:22:22 +0200

    add dir parameter with clap

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use chrono::{DateTime, Local};
+use clap::ArgAction;
 use der::{Decode, Tagged};
 use std::fs::{self};
+use std::path::PathBuf;
 use std::time::SystemTime;
 use std::{borrow::BorrowMut, collections::BTreeMap};
 use tap::Pipe;
@@ -18,24 +20,42 @@ struct Row {
 }
 
 fn main() {
-    let profiles_dir = dirs::home_dir()
-        .unwrap()
-        .join("Library/MobileDevice/Provisioning Profiles");
+    let dir_arg = clap::Arg::new("directory")
+        .short('d')
+        .long("dir")
+        .action(ArgAction::Set)
+        .value_parser(clap::value_parser!(PathBuf))
+        .value_name("DIR")
+        .default_value( "~/Library/MobileDevice/Provisioning Profiles");
+    
+    let matches = clap::Command::new(clap::crate_name!())
+        .author(clap::crate_authors!())
+        .arg(&dir_arg)
+        .get_matches();
 
-    let paths = fs::read_dir(profiles_dir)
+    let input_dir_str = paths_as_strings::encode_path({
+        let dir_arg_id = dir_arg.get_id().as_str();
+        let path_buf = matches.get_one::<PathBuf>(&dir_arg_id).unwrap();
+        path_buf
+    });
+    let input_dir_expanded = shellexpand::tilde(&input_dir_str).into_owned();
+
+    println!("scanning directory: {:?}", input_dir_expanded);
+
+    let paths = fs::read_dir(PathBuf::from(input_dir_expanded))
         .unwrap()
         .map(|dir_entry| dir_entry.unwrap().path())
         .filter_map(|path| {
             if path
-                .extension()
-                .map_or(false, |ext| ext == "mobileprovision")
+            .extension()
+            .map_or(false, |ext| ext == "mobileprovision")
             {
                 Some(path)
             } else {
                 None
             }
         });
-
+    
     println!();
 
     let rows = paths.map(|path| {
