43efd91e3374f8687b37937e4f13c7e01441f6ba
juwens <>
2024-11-24 13:50:42 +0100

    unify dir handling

--- src/args.rs
+++ src/args.rs
@@ -1,11 +1,22 @@
 use clap::{Parser, Subcommand, ValueEnum};
-use std::{ffi::OsString, path::{Path, PathBuf}, str::FromStr};
+use std::{ffi::OsString, path::PathBuf};
 
 const XC_16_DIR: &str = "~/Library/Developer/Xcode/UserData/Provisioning Profiles";
 const XC_15_DIR: &str = "~/Library/MobileDevice/Provisioning Profiles";
 
+// https://github.com/clap-rs/clap/blob/10c29ab75dfc15ae8ae7218d699de5a2b57afabe/clap_builder/src/output/help_template.rs#L64
+// https://github.com/clap-rs/clap/blob/10c29ab75dfc15ae8ae7218d699de5a2b57afabe/tests/derive/utils.rs#L11
+const HELP_TEMPLATE: &str = "\
+{before-help}{name} {version}
+{author}
+{author-with-newline}{about-with-newline}
+{usage-heading} {usage}
+
+{all-args}{after-help}\
+    ";
+
 #[derive(Parser, std::fmt::Debug)]
-#[command(version, about, long_about = None)]
+#[command(version, about, long_about = None, help_template(HELP_TEMPLATE))]
 pub struct MyCliArgs {
     #[command(subcommand)]
     pub command: Commands,
@@ -14,13 +25,9 @@ pub struct MyCliArgs {
     /// Can be necessary/helpful, when Xcode changed the path, and pputil wasn't updated yet.
     /// Should be not needed usually.
     /// Usage: `cargo run -- --dir "/tmp/" list`
-    #[arg(short('d'), long("dir"))]
-    #[clap(hide=true)]
-    pub custom_dir: Vec<String>,
-
-    // the default and/or expanded paths
-    #[clap(skip)]
-    pub dirs_ex: Vec<XcProvisioningProfileDir>,
+    #[arg(short('d'), long("dir"), global(true))]
+    #[clap(hide = true)]
+    custom_dir: Vec<String>,
 
     #[arg(
         long,
@@ -30,6 +37,33 @@ pub struct MyCliArgs {
         help = "Increase logging verbosity",
     )]
     verbose: u8,
+}
+
+impl MyCliArgs {
+    pub fn actual_dirs(&self) -> Vec<XcProvisioningProfileDir> {
+        let relative_dirs = if self.custom_dir.is_empty() {
+            vec![
+                XcProvisioningProfileDir {
+                    relative_path: XC_16_DIR.into(),
+                    kind: XcProvisioningProfileDirKind::Xc16,
+                },
+                XcProvisioningProfileDir {
+                    relative_path: XC_15_DIR.into(),
+                    kind: XcProvisioningProfileDirKind::Xc15,
+                },
+            ]
+        } else {
+            self.custom_dir
+                .iter()
+                .map(|x| XcProvisioningProfileDir {
+                    relative_path: OsString::from(x),
+                    kind: XcProvisioningProfileDirKind::Custom,
+                })
+                .collect()
+        };
+
+        relative_dirs
+    }
 }
 
 impl std::fmt::Debug for Commands {
@@ -43,20 +77,21 @@ impl std::fmt::Debug for Commands {
 
 #[derive(Clone, Debug)]
 pub struct XcProvisioningProfileDir {
-    pub path: std::ffi::OsString,
+    pub relative_path: std::ffi::OsString,
     pub kind: XcProvisioningProfileDirKind,
 }
 impl XcProvisioningProfileDir {
-    pub(crate) fn path_as_path(&self) -> PathBuf {
-        let expanded = shellexpand::tilde(&self.path.to_str().unwrap()).to_string();
-        let osstr: OsString = OsString::from_str(&expanded).unwrap();
-        osstr.into()
+    pub(crate) fn absolute_path(&self) -> PathBuf {
+        let binding = self.relative_path.to_string_lossy().into_owned();
+        let absolute = shellexpand::tilde(binding.as_str());
+        let s = absolute.into_owned();
+        PathBuf::from(s)
     }
 }
 impl std::fmt::Display for XcProvisioningProfileDir {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("")
-            .field("path", &self.path)
+            .field("path", &self.relative_path)
             .field("xc", &self.kind)
             .finish()
     }
@@ -76,9 +111,7 @@ pub struct ListCompactArgs {
 }
 
 #[derive(Parser, Debug)]
-pub struct ListExtendedArgs {
-
-}
+pub struct ListExtendedArgs {}
 
 #[derive(Subcommand)]
 #[command()]
@@ -113,33 +146,12 @@ pub enum XcProvisioningProfileDirKind {
     Xc15 = 1,
     /// greater or equal
     Xc16 = 2,
+    Custom,
 }
 
 #[allow(clippy::assigning_clones)]
 pub fn get_processed_args() -> MyCliArgs {
-    let mut args = MyCliArgs::parse();
-    let input_dirs_expanded: &Vec<String> = &args
-        .custom_dir
-        .iter()
-        .map(|x| shellexpand::tilde(&x).into())
-        .collect();
-
-    // plausibility check for dirs
-    for dir in input_dirs_expanded {
-        let input_dir_path = Path::new(&dir);
-        assert!(input_dir_path.is_dir());
-        assert!(input_dir_path.is_absolute());
-    }
-
-    args.custom_dir = input_dirs_expanded.clone();
-    
-    if args.custom_dir.is_empty() {
-        args.dirs_ex = vec![
-            XcProvisioningProfileDir{path: XC_16_DIR.into(), kind: XcProvisioningProfileDirKind::Xc16},
-            XcProvisioningProfileDir{path: XC_15_DIR.into(), kind: XcProvisioningProfileDirKind::Xc15},
-        ];
-        args.custom_dir = args.dirs_ex.iter().map(|x|x.path.to_string_lossy().to_string()).collect();
-    }
+    let args = MyCliArgs::parse();
 
     if args.verbose > 0 {
         dbg!(&args);
